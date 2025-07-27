e2a8b7b10c436a28edb02a850bf5f0c1a585dce5
Larissa Vongsuvanh <z5592397@ad.unsw.edu.au>
2025-07-27 22:40:13 +1000

    fixed analysis_state bug

--- bone_age/frontend/app/home.py
+++ bone_age/frontend/app/home.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 
 import streamlit as st
+from app import helpers
 
 def main_ui():
     st.title("Bone-Ager")
     st.subheader("An automatic paediatric bone age assessment tool")
 
-    st.text_input("What is your name?", key="name")
-
     option = st.selectbox('What is the sex?', ('Female', 'Male', 'Unknown'))
     st.write('You selected:', option)
         
     uploaded = st.file_uploader(
-        "Upload X-ray image (JPEG, PNG, or DICOM)", 
-        type=["jpeg", "jpg", "png", "dcm"]
+        "Upload X-ray image (JPEG, JPG, PNG, or DICOM)", 
+        type=["jpeg", "jpg", "png", "dcm"],
+        accept_multiple_files=True,
+        on_change=helpers.reset_analysis
     )
 
     if uploaded:
