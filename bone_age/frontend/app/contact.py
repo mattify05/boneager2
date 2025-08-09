fbc12d4612a7dc24833152f1b5659b6f1a7f8657
Larissa Vongsuvanh <z5592397@ad.unsw.edu.au>
2025-08-09 17:25:11 +1000

    added team profiles

--- bone_age/frontend/app/contact.py
+++ bone_age/frontend/app/contact.py
@@ -5,11 +5,19 @@ import streamlit as st
 def render_contact():
     st.title("Contact Us")
     st.write("""
-    Want to get in touch? We'd love to hear from you. Here's how you can reach us.
-    
-    - **GitHub:** [Project Repository](https://github.com/jjjaden-hash/DESN2000-BINF-M13B_GAMMA/tree/main)
-             
-    
+        Want to get in touch? We'd love to hear from you. Here's how you can reach us.
+        
+        - **GitHub:** [Project Repository](https://github.com/jjjaden-hash/DESN2000-BINF-M13B_GAMMA/tree/main)
+        - **University:** University of New South Wales (UNSW), Course: DESN2000 - Bioinformatics Engineering
+                
+        This course was developed by university students as part of our coursework.
+        For more information or to contribute, feel free to explore the repository!
+        
+        **Meet the Team** *(LinkedIn profiles below)*
+        - [Jaden Choo](https://www.linkedin.com/in/jaden-choo-7a38682bb/) - Backend Developer
+        - [Nicole Consolacion](https://www.linkedin.com/in/nicolecons/) - Product Documentation Lead
+        - [Matthew Thien](https://www.linkedin.com/in/matthew-thien-39074a271/) - Technical Writer
+        - [Larissa Vongsuvanh](https://www.linkedin.com/in/larissa-vongsuvanh-237674313/) - Frontend Developer
     """)
         
         
